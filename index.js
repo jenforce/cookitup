9d8875f4108f8136dde66971067576789bc39db0
Seth Keel <seth.keel@gmail.com>
2016-10-06 19:30:45 -0400

    updated first card

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-angular.module('CookItUp', ['ngMaterial'])
+// angular.module('CookItUp', ['ngMaterial'])
 
-.controller('AppCtrl', function($scope) {
-  $scope.imagePath = 'assets/img/exampleimg.jpeg';
-})
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
-  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
-  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
-  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
-});
+// .controller('AppCtrl', function($scope) {
+//   $scope.imagePath = 'assets/img/exampleimg.jpeg';
+// })
+// .config(function($mdThemingProvider) {
+//   $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+//   $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+//   $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+//   $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+// });
