4bd9d92f94ba16f6a1404d93939013aea371dae8
Coffeeteer <tgrieshiemer@gmail.com>
2016-10-11 18:36:21 -0400

    trying to get FB Auth to work in Heroku

--- config/auth.js
+++ config/auth.js
@@ -2,14 +2,14 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : process.env.FACEBOOKAPPID || '306010069772025', // your App ID
-        'clientSecret'  : process.env.FACEBOOKSECRET || 'a19fd4445756be1951d83dcf187f2eb5', // your App Secret
-        'callbackURL'   : process.env.FACEBOOKCALLBACKURL || 'http://localhost:3010/auth/facebook/callback'
+        'clientID'      : process.env.FACEBOOK_APP_ID || '306010069772025', // your App ID
+        'clientSecret'  : process.env.FACEBOOK_APP_SECRET || 'a19fd4445756be1951d83dcf187f2eb5', // your App Secret
+        'callbackURL'   : process.env.FACEBOOK_CALLBACK_URL || 'http://localhost:3010/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey'       : process.env.CONSUMER_KEY || 'YwCkdIDWuANTQukh4pYt39dPI',
-        'consumerSecret'    : process.env.CONSUMER_SECRET || 'MXfpjOeYOH2Nq9vxXkNB4mRUhqUoTEoIid8gUxoZDJuq81u6MV',
+        'consumerKey'       : process.env.TWITTER_CONSUMER_KEY || 'YwCkdIDWuANTQukh4pYt39dPI',
+        'consumerSecret'    : process.env.TWITTER_CONSUMER_SECRET || 'MXfpjOeYOH2Nq9vxXkNB4mRUhqUoTEoIid8gUxoZDJuq81u6MV',
         'callbackURL'       : process.env.callbackURL || 'http://127.0.0.1:3010/auth/twitter/callback'
     },
 
